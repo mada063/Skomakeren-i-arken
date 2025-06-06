75a7f0e042b4bf627ef2d3d4aa30232f068aadfd
Mathias <mateikdal@gmail.com>
2025-06-06 15:08:58 +0200

    Import av font, farger. Oprettet filer for Footer, Header, Tekst seksjon og Hero

--- app/bil-nokler/page.tsx
+++ app/bil-nokler/page.tsx
@@ -3,7 +3,7 @@ import Test from "@/components/Test"
 export default function BilNokler() {
     return (
         <div>
-            <Test category="bil-nokler" />
+            
         </div>
     )
 }
