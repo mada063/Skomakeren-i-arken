ce9d705b13498223a7ee3054f0bd26dd386bd493
Mathias <mateikdal@gmail.com>
2025-06-11 15:28:05 +0200

    Footer og knapp endringer

--- app/home/components/InfoSection.tsx
+++ app/home/components/InfoSection.tsx
@@ -8,11 +8,12 @@ const InfoSection = () => {
         <div>
             <h2 className='text-7xl uppercase font-semibold'>Hvorfor oss?</h2>
             <p className='font-sans mt-4'>Eksempeltekst pioakpdsofakpsdfkoaspdo aspdfko aspdofk pasddkofpo ooasdpfasdof osdodaofoasodfo oooo  oosam,mvb bsåp,åpqdlksdv naldskmvåwqåpåf åpsfåasdckmavsdvpaom</p>
-            <a className='mt-8'>
-                <button className="mt-8 bg-custom-blue text-white px-6 py-3 rounded-lg transition-all duration-300 hover:bg-opacity-80 hover:scale-105 hover:shadow-lg">
-                    Kontakt oss
-                </button>
-            </a>
+            <a
+          href="https://www.skomakeren.no/"
+          className="bg-custom-blue text-white px-6 py-3 rounded-lg transition-all duration-300 hover:bg-opacity-80 hover:scale-105 hover:shadow-lg inline-block"
+        >
+          Kontakt oss
+        </a>
         </div>
       </div>
     </div>
--- app/home/components/LandingHero.tsx
+++ app/home/components/LandingHero.tsx
@@ -9,10 +9,11 @@ export default function LandingHero() {
           Skomakeren<br />i Arken
         </h1>
         <h4 className="text-xl uppercase text-white">Åsane senter, Bygg B</h4>
-        <a href="https://www.skomakeren.no/">
-          <button className="bg-custom-blue text-white px-6 py-3 rounded-lg transition-all duration-300 hover:bg-opacity-80 hover:scale-105 hover:shadow-lg">
-            Kontakt oss
-          </button>
+        <a
+          href="https://www.skomakeren.no/"
+          className="bg-custom-blue text-white px-6 py-3 rounded-lg transition-all duration-300 hover:bg-opacity-80 hover:scale-105 hover:shadow-lg inline-block"
+        >
+          Kontakt oss
         </a>
       </main>
     </div>
